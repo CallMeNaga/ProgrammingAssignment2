a676438efd0681a5ea9edfc958acd90ebd41bd84
CallMeNaga <gorla.indra@gmail.com>
2015-12-27 13:50:52 +0800

    edited cachematrix.R file and pasted my assignment code in it

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,32 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x=matrix()){
+	Mat_inv<-NULL
+	set<-function(y){
+		x<<-y
+		m<<-NULL
+		}
+	get <- function(){
+		x
+		}
+	setInverse<-function(inverse){
+		Mat_inv<<-inverse
+		}
+	getInverse<-function(){
+		Mat_inv
+		}
+list(set=set,get=get,setInverse=setInverse, getInverse=getInverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+cacheSolve <- function(x,...){
+	Mat_inv<-x$getInverse()
+	if(!is.null(Mat_inv)){
+		message("getting cached data")
+		return(Mat_inv)
+		}
+	data<-x$get()
+	Mat_inv<-solve(data,...)
+	x$setInverse(Mat_inv)
+	Mat_inv	
+	}
